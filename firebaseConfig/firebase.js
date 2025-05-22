97bfa62294a1364b7cc05cd201e96ae7ea04a084
Rahul Vandrangi <rahulvandrangi1k@gmail.com>
2025-05-22 20:41:12 +0530

    Delete firebaseConfig directory

--- firebaseConfig/firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyClunAUe7LFv__lXtool3Cjy8VEwteYJY0",
-  authDomain: "agronet-f1e52.firebaseapp.com",
-  projectId: "agronet-f1e52",
-  storageBucket: "agronet-f1e52.firebasestorage.app",
-  messagingSenderId: "1062026473422",
-  appId: "1:1062026473422:web:4981f65642e33ff3cb059f",
-  measurementId: "G-2YENJTJ5YZ",
-};
